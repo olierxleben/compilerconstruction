e23b458071fb14ffcbf5b5c18220ee8212a8e80f
Oliver Erxleben <oliver.erxleben@me.com>
2013-07-26 11:19:51 +0200

    added support for index files. Rackserver now serves folders with css, js and images

--- hausarbeit/messdaten/server.rb
+++ hausarbeit/messdaten/server.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
 
-# USAGE: ./server.rb path/to/the/file
+# USAGE: ./server.rb path/to/the/root/dir
 
 require 'rubygems'
 require 'rack' # rack it up
 
 serve = Rack::Builder.new do
-  run Rack::File.new( ARGV[0] )
+ # run Rack::Directory.new( ARGV[0] )
+ use Rack::Static, 
+   :urls => ["/images", "/js", "/css"],
+   :root => ARGV[0],
+   :index => 'index.html'
+ run Rack::File.new(ARGV[0])
 end
 
 # by default WEBrick doesen`t listen to sigkill, cause the script runs in a new session. 
@@ -14,7 +19,5 @@ Signal.trap('INT') {
   Rack::Handler::WEBrick.shutdown
 }
 
-
 # takeoff
-
 Rack::Handler::WEBrick.run(serve, :port => 8080, 'Contetn-Type'=>'text/html')
