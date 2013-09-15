58a2f3fbe0660293f67d9aa6a8534e61b5f22584
Sergej <sergej@sergej-VirtualBox.(none)>
2013-09-15 13:08:23 +0200

    Added function shortHandColour to optimize the colour hex value.

--- hausarbeit/app/optimizer.c
+++ hausarbeit/app/optimizer.c
@@ -3,10 +3,34 @@
 #include <string.h>
 #include <stdio.h>
 
+/*
+* Set hex pair values to one value #336699 -> #369 or #34228 -> #3428
+* char ** because I need the adress of the pointer to set the new value
+*/ 
+static void shortHandColour(char** val){
+    int i = 1;  // position in value
+    int j = 1;  // position in newValue
+    char *value = *val;
+    char newValue[sizeof(value)/sizeof(char)] = {0};    // Init
+    
+    // Isn't a hex value don't do anything
+    if(value && value[0] == '#'){
+        newValue[0] = '#';
+        for(; i < (sizeof(value)/sizeof(char)-1); i++, j++){
+            newValue[j] = value[i];   
+            if(value[i] == value[i+1]){
+                i++;
+            }           
+        }    
+    }
+    *val = strdup(newValue);
+}
+
 css_RuleList optimize(css_RuleList list) {
 	// merge nodes with same selector
 	return mergeNodes(list);
 }
+
 css_RuleList mergeNodes(css_RuleList list) {	
 	css_RuleList newRules = NULL;	
 	css_Rule tmpRule = list->rule;
@@ -20,13 +44,8 @@ css_RuleList mergeNodes(css_RuleList list) {
 		newRule = mergeToNewRule(tmpRule, NULL, currSel);
 		while(tmpList) {
 			if(containsSelector(tmpList->rule->selectorList, currSel)) {
-				//css_Rule old = newRule;
 				newRule = mergeToNewRule(newRule, tmpList->rule, currSel);
-				
-				
-				//free(old); // TODO MAL gucken wegen speicherlecks
-			}
-			
+			}		
 		
 			tmpList = tmpList->next;
 		}
@@ -57,6 +76,7 @@ css_Rule mergeToNewRule(css_Rule rule1, css_Rule rule2, css_Selector selector) {
 	if(rule1){
 	    tmpList = rule1->declarationList;
 	    while(tmpList) {
+	        shortHandColour(&(tmpList->declaration->dec_val));
 		    css_Declaration tmpDec = create_CSSDeclaration(tmpList->declaration->dec_key, tmpList->declaration->dec_val);
 		    decList = create_CSSDeclarationList(tmpDec, decList);
 		    tmpList = tmpList->next;
@@ -66,6 +86,7 @@ css_Rule mergeToNewRule(css_Rule rule1, css_Rule rule2, css_Selector selector) {
 	if(rule2){
 	    tmpList = rule2->declarationList;
 	    while(tmpList) {
+	        shortHandColour(&(tmpList->declaration->dec_val));
 		    css_Declaration tmpDec = create_CSSDeclaration(tmpList->declaration->dec_key, tmpList->declaration->dec_val);
 		    decList = create_CSSDeclarationList(tmpDec, decList);
 		    tmpList = tmpList->next;
@@ -74,4 +95,5 @@ css_Rule mergeToNewRule(css_Rule rule1, css_Rule rule2, css_Selector selector) {
 	
 	return create_CSSRule(create_CSSSelectorList(selector, NULL), decList);
 }
+
 
